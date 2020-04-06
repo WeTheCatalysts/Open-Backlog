a26368cb419129d9e0893fec62bcc2ccb401b2b1
Chris Thorpe <drchristhorpe@googlemail.com>
2020-04-06 09:01:15 +0100

    Work to make the app run on Google Cloud (Appengine)

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,6 @@
+export GOOGLE_APPLICATION_CREDENTIALS="../pipeline-frontend-25e3d1b8b3d0.json"
 export FLASK_APP=main.py
-export APPLICATION_SETTINGS=../settings.cfg
+#export APPLICATION_SETTINGS=../settings.cfg
 export FLASK_DEBUG=1
 export LOCAL_DATA=True
 #flask run
